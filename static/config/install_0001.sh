25d9f9f978b6aa432928116657151d4455e2f011
王璇 <wangxuan2022@mail.nwpu.edu.cn>
2025-10-10 17:08:39 +0800

    .

--- static/config/install_0001.sh
+++ static/config/install_0001.sh
@@ -5,6 +5,7 @@ echo ". ~/spack/share/spack/setup-env.sh" >> ~/.bashrc && source .bashrc
 . ~/spack/share/spack/setup-env.sh
 
 # 先安装gcc11.4.0，再在指定的虚拟环境中安装fenics以及其他组件
-spack install gcc@11.4.0 && spack load gcc@11.4.0 && spack compiler find && spack env create kokkos && \
+spack install -j80 gcc@11.4.0 && spack load gcc@11.4.0 && spack compiler find && spack env create kokkos && \
   wget https://www.pengfeima.cn/config/spack_0001.yaml -O spack.yaml && mv spack.yaml ~/spack/var/spack/environments/kokkos && \
-  spack env activate -p kokkos && spack concretize -f && spack install
+  spack env activate -p kokkos && spack concretize -f && spack install -j80
+
